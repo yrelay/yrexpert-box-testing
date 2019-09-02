7f1cf4c70d8163a4a1a8023b2ef6b7c2490ee520
irelay <irelay@free.fr>
2019-09-02 17:23:51 +0200

    0.0.0 - Màj auto

--- gtm/installerGTM.sh
+++ gtm/installerGTM.sh
@@ -51,7 +51,6 @@ else
   fi
 
   # S'assurer que le répertoire existe pour les liens vers GT.M actuel
-  sudo mkdir -p $gtmroot/previous_`date -u +%Y-%m-%d:%H:%M:%S`
   sudo mkdir -p $gtmcurrent
   
   # Télécharger et installer GT.M, y compris UTF-8 mode
